4a00ac5af4b7ed45e6a0ef701851f98743a6afde
deyvidgt <papagaio07@hotmail.com>
2026-05-19 12:43:20 -0300

    att 27

--- streamlit/db.py
+++ streamlit/db.py
@@ -14,7 +14,7 @@ def _hash(senha: str) -> str:
 @st.cache_resource
 def get_client() -> Client:
     url = "https://avbfpesmjghiindghnzx.supabase.co"
-    key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF2YmZwZXNtamdoaWluZGdobnA0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDiLCJleHAiOjIwNjM2NjY4MDh9.BES-GQT-yzX7fNzvLaIFHw98gwZEGHDRzKNZIXbSvCc"
+    key = "sb_publishable_ZzQ17v4ImJB1Kiq_Ky5K_Q_HSQFNP7w"
     return create_client(url, key)
 
 
